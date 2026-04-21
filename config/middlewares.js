6687fbc180df12b0e7ba7641213cbcdd236964a6
bang6614 <123051802+bang6614@users.noreply.github.com>
2026-04-22 03:04:05 +0800

    Update middlewares.js
    Add CORS whitelist for bnjdesign.com.tw

--- config/middlewares.js
+++ config/middlewares.js
@@ -2,7 +2,23 @@ module.exports = [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      origin: [
+        'https://bnjdesign.com.tw',
+        'https://www.bnjdesign.com.tw',
+        'http://bnjdesign.com.tw',
+        'http://www.bnjdesign.com.tw',
+        'http://localhost:3000',
+        'http://localhost:8000',
+        'http://localhost:5500',
+        'http://127.0.0.1:5500'
+      ],
+      headers: '*'
+    }
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
